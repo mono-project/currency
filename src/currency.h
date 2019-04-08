0b94b63503de3b00644894c99c56307f51d1c93d
ClashLuke <luke@clashproject.com>
2019-04-08 10:45:09 +0200

    Moved up

---
+++ src/currency.h
@@ -0,0 +1,8 @@
+#include <stdint.h>
+
+uint8_t* buildTx(uint8_t* in, uint8_t* out, uint64_t amount);
+uint8_t* buildCoinbaseTx(uint8_t* out, uint32_t amount);
+void addNonce(uint8_t* tx, uint64_t nonce);
+uint8_t* constructRawTx(uint8_t* in, uint8_t* out, uint64_t amount, uint64_t nonce);
+void signRawTx(uint8_t* rawTx, uint8_t* privateKey);
+uint8_t* blockTemplate(uint8_t* minerUserName, uint8_t* transactions, uint8_t* signatures, uint8_t* prevBlockHash, uint32_t txCount, uint32_t difficulty, uint32_t fee, uint32_t timestamp);
