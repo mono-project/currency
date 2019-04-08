2567b6dfdadf7f7e43014a6da9e54bf1d5d1c861
ClashLuke <luke@clashproject.com>
2019-04-08 12:05:56 +0200

    Added validation header

---
+++ src/interface/validate.h
@@ -0,0 +1,4 @@
+void validateBlockSignature(uint8_t* transactions, uint8_t* signature);
+uint8_t* hashBlock(uint8_t* block, uint32_t height);
+uint8_t validateHash(uint8_t* hash, uint64_t difficulty);
+uint8_t validateBlockHash(uint8_t* block, uint32_t height, uint64_t difficulty);
