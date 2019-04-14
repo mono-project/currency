4a0325f2e1935ced7fc96a63a6f19d4b3e147a7d
ClashLuke <luke@clashproject.com>
2019-04-14 21:47:57 +0200

    getPeerList

--- src/Database/read.c
+++ src/Database/read.c
@@ -3,22 +3,25 @@
 #include "../config.h"
 #include "difficulty.h"
 
-char folder = calloc(256,1);
-snprintf(folder, 256, "~/.%s/", CURRENCY_NAME);
-
 // Open globally needed files
 /*
 snprintf(buf, 256, folder, "txDB");
 FILE* txDB = fopen(buf, "r");
 snprintf(buf, 256, folder, "headerDB");
 FILE* headerDB = fopen(buf, "r");
-snprintf(buf, 256, folder, "peerList");
-FILE* peerList = fopen(buf, "r");
 snprintf(buf, 256, folder, "accountDB");
 FILE* accountDB = fopen(buf, "r");
 free(buf);
 */
+
+char* getFolder(){
+	char* folder = calloc(256,1);
+	snprintf(folder, 256, "~/.%s/", CURRENCY_NAME);
+	return(folder);
+}
+
 uint64_t getDifficulty(uint32_t height){
+	char* folder = getFolder();
 	char* fileName = (char*)malloc(256);
 	snprintf(fileName, 256, folder, "diffDB");
 	FILE* diffDB = fopen(fileName, "r");
@@ -52,6 +55,7 @@ uint64_t getDifficulty(uint32_t height){
 }
 
 uint8_t* getTx(uint64_t txNumber){
+	char* folder = getFolder();
 	char* fileName = (char*)malloc(256);
 	char* tx = (char*)malloc(20);
 	snprintf(fileName, 256, folder, "txDB");
@@ -67,6 +71,7 @@ uint8_t* getTx(uint64_t txNumber){
 }
 
 uint8_t* getBlockTx(uint32_t height){
+	char* folder = getFolder();
 	char* fileName = (char*)malloc(256);
 	snprintf(fileName, 256, folder, "txDB");
 	FILE* txDB = fopen(fileName, "r");
@@ -91,5 +96,23 @@ uint8_t* getBlockTx(uint32_t height){
 	return(txs);
 }
 
+uint32_t* getPeerList(){
+	char* folder = getFolder();
+	char* fileName = (char*)malloc(256);
+	uint64_t* ip = (uint64_t*)malloc(8); //255.255.255.255:65536 - B.B.B.B:BB -> 6 Byte
+	snprintf(fileName, 256, folder, "peerList");
+	FILE* peerList = fopen(fileName, "r");
+	free(fileName);
 
-
+	uint32_t peerCount = 0;
+	while(fgets(ip, 8, peerList)) peerCount++;
+	fseek(txNumDB, 0, SEEK_SET)
+	uint64_t* ips = (uint64_t*)malloc(peerCount*8);
+	for(uint32_t i=0;i<peerCount;i++){
+		fgets(ip, 8, peerList)
+		for(uint8_t j=0;j<8;j++){
+			ips[i] += ip[j] << (8*j);
+		}
+	}
+	return(ips);
+}
