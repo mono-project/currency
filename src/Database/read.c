eb9a518a38ed2c3d8defa42334a6c87f66c3077a
ClashLuke <luke@clashproject.com>
2019-04-14 21:03:10 +0200

    Added getTx

--- src/Database/read.c
+++ src/Database/read.c
@@ -19,7 +19,7 @@ FILE* accountDB = fopen(buf, "r");
 free(buf);
 */
 uint64_t getDifficulty(uint32_t height){
-	char fileName = malloc(256,1);
+	char* fileName = malloc(256,1);
 	snprintf(fileName, 256, folder, "diffDB");
 	FILE* diffDB = fopen(fileName, "r");
 	snprintf(fileName, 256, folder, "headerDB");
@@ -38,9 +38,9 @@ uint64_t getDifficulty(uint32_t height){
 	if(headerCnt < 256 && diffCnt < 256) return 1;
 	fseek(diffDB, (diffCnt-256)*8, SEEK_SET);	
 	fseek(headerDB, (headerCnt-256)*BLOCKHEADER_SIZE, SEEK_SET);	
-	fgets(diffBuf, 8, diffDB)
-	fgets(headerBuf, BLOCKHEADER_SIZE, headerDB)
 	for(uint16_t j=0;j<256;j++){
+		fgets(diffBuf, 8, diffDB)
+		fgets(headerBuf, BLOCKHEADER_SIZE, headerDB)
 		for(uint8_t i=0;i<4;i++){
 			timestamps[j] += headerBuf[i+j*BLOCKHEADER_SIZE] << (8*i);
 		}
@@ -49,7 +49,20 @@ uint64_t getDifficulty(uint32_t height){
 		}
 	}
 	return(diff(difficulties, timestamps, -1));
- 
+}
+
+uint8_t* getTx(uint64_t txNumber){
+	char* tx = malloc(20,1);
+	snprintf(fileName, 256, folder, "txDB");
+	FILE* txDB = fopen(fileName, "r");
+	free(fileName);
+
+	uint64_t txCnt = 0;
+	while(fgets(tx, 20, txDB)) txCnt++;
+	if(txCnt < txNumber) return 0;
+	fseek(txDB, txNumber*20, SEEK_SET);	
+	fgets(tx, 20, txDB)
+	return(tx);
 }
 
 
