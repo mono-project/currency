5ceb08f3dd51bd1e721d0b7afa50f69de1b67eab
ClashLuke <luke@clashproject.com>
2019-04-14 19:04:20 +0200

    Updated config (blocktime variable)

--- src/config.h
+++ src/config.h
@@ -11,5 +11,6 @@
 #define QUICK_VERIFY    // Comment this line if quick verification should not be used in the miner
 			// Pro: Hash verification is faster. Much faster.
 			// Con: Hash verification requires much more L1/L2 cache.
+#define BLOCKTIME 120
 
 #endif
