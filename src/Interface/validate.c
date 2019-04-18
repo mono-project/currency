0b12f1ad6d8d805031053d938c15c2af55325106
ClashLuke <luke@clashproject.com>
2019-04-18 12:54:11 +0200

    validateTransactions

--- src/Interface/validate.c
+++ src/Interface/validate.c
@@ -11,6 +11,31 @@ uint8_t validateBlockSignature(uint8_t* block){
 	//TODO if(missmatch) return 0
 }
 
+uint8_t processTransaction(uint8_t* transaction){
+	if(*transaction != 255){
+		uint64_t prevFunds = getFundsForUsername(&transaction[1]);
+		if(!prevFunds) return 0 + *transaction;
+		return 128 + *transaction
+		// Remove funds from X and adding it to Y
+	} else {
+		; // Add user to database
+		return 232; // 128 + 104
+	}
+	
+}
+
+uint8_t validateTransactions(uint8_t* block, uint32_t txCount){
+	if(!validateBlockSignature(block)) return 0;
+	uint64_t pos = 180;
+	for(uint32_t i=0;i<txCount;i++){
+		uint8_t status = processTransaction(&block[pos]);
+		if(!(status>>7)) return 0;
+		pos += status&0x7F;
+	}
+	return 1;
+	
+}
+
 uint8_t* hashBlock(uint8_t* block, uint32_t height){
 	uint64_t* hash_64 = (uint64_t*)malloc(64);
 	uint8_t*  hash    = (uint8_t*)hash_64;
@@ -18,7 +43,7 @@ uint8_t* hashBlock(uint8_t* block, uint32_t height){
 		blakeln(block, 180, hash);
 	} else {
 		uint32_t txCount = 0;
-		for(uint8_t i=0; i<32;i+=8) txCount += block[i+160]<<i;
+		for(uint8_t i=0; i<32;i+=4) txCount += block[i+160]<<(8*i);
 		squash_light_api(block, 180+txCount*24, hash, height);
 	}
 	return hash;
@@ -38,5 +63,5 @@ uint8_t validateBlock(uint8_t* block, uint32_t height){
 	for(uint8_t i=0; i<4; i++) timestamp += block[i+164]<<(8*i);
 	uint64_t difficulty = getDifficultyForTimestamp(timestamp, height);
 	if(!validateBlockHash(block, height, difficulty)) return 0;
-	if(!validateBlockSignature(block)) return 0;	
+	if(!validateTransactions(block)) return 0;
 }
