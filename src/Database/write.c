521925a6952e56ac2c730b1bf8b7269f9551f655
ClashLuke <luke@clashproject.com>
2019-04-19 15:32:15 +0200

    setFundsForUsername

---
+++ src/Database/write.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+#include "../config.h"
+#indlude "read.h"
+
+uint8_t setFundsForUsername(uint8_t* username, uint64_t funds){
+	uint8_t   good     = 1;
+	char*     folder   = getFolder();
+	char*     fileName = (char*)malloc(256);
+	uint64_t* user     = (uint64_t*)malloc(8);
+	uint64_t* funds    = (uint64_t*)malloc(8);
+	uint8_t*  funds_8  =  (uint8_t*)&funds;
+	snprintf(fileName, 256, folder, "userDB");
+	FILE* userDB = fopen(fileName, "r");
+	snprintf(fileName, 256, folder, "wealthDB");
+	FILE* wealthDB = fopen(fileName, "w");
+	free(fileName);
+	free(folder);
+	while(fgets(user, 8, userDB)){
+		if(user[0] != username[0]
+		|| user[1] != username[1]
+		|| user[2] != username[2]
+		|| user[3] != username[3]
+		|| user[4] != username[4]
+		|| user[5] != username[5]
+		|| user[6] != username[6]
+		|| user[7] != username[7]){
+			fseek(wealthDB,8,SEEK_CUR);
+			continue;
+		}
+		fprintf(wealthDB, funds_8, 8);
+		return 1;
+	}
+	return 0;
+}
+
+
