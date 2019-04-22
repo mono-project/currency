f31f069f4079687fc9dcbd25b2d57dcf2bf13f0d
ClashLuke <luke@clashproject.com>
2019-04-22 08:34:03 +0200

    getFundchangeForUsername

--- src/Database/read.c
+++ src/Database/read.c
@@ -224,6 +224,41 @@ uint64_t getFundsForUsername(uint8_t* username){
 	fclose(wealthDB);
 	return 0;
 }
+uint64_t getFundchangeForUsername(uint8_t* username){
+	uint8_t good = 1;
+	char* folder = getFolder();
+	char* fileName = (char*)malloc(256);
+	uint64_t* user = (uint64_t*)malloc(8);
+	uint64_t* funds = (uint64_t*)malloc(8);
+	uint64_t  out = 0;
+	snprintf(fileName, 256, folder, "userDB");
+	FILE* userDB = fopen(fileName, "r");
+	snprintf(fileName, 256, folder, "wealthChangeDB");
+	FILE* wealthChangeDB = fopen(fileName, "r");
+	free(fileName);
+	free(folder);
+	while(fgets(user, 8, userDB)){
+		fgets(funds, 8, wealthChangeDB);
+		if(user[0] != username[0]
+		|| user[1] != username[1]
+		|| user[2] != username[2]
+		|| user[3] != username[3]
+		|| user[4] != username[4]
+		|| user[5] != username[5]
+		|| user[6] != username[6]
+		|| user[7] != username[7]) continue;
+		fclose(userDB);
+		fclose(wealthChangeDB);
+		out = *funds;
+		free(funds);
+		return(out);
+	}
+	free(user);
+	free(funds);
+	fclose(userDB);
+	fclose(wealthChangeDB);
+	return 0;
+}
 
 uint64_t* getPubKeys(){
 	char* folder = getFolder();
