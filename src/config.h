34a995d7304f58ecb47d01dca35e78db1c5a7d22
ClashLuke <luke@clashproject.com>
2019-06-14 20:35:58 +0200

    Maximum -> Minimum reward

--- src/config.h
+++ src/config.h
@@ -2,7 +2,7 @@
 #define CONF
 
 #define CURRENCY_NAME "mono"   // Lowercase, written in quotation marks
-#define BASE_REWARD 0x40000000 // Maximum block reward, multiplied with REWARD_FUNCTION(difficulty)
+#define BASE_REWARD 1000000 // Minimum block reward, multiplied with REWARD_FUNCTION(difficulty)
 
 #ifdef GET_REWARD_FUNCTION
   #define REWARD_FUNCTION(x) pow(log2(x), 2.0)
