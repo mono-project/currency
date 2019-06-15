4266472b637a63ec73f48fefa416e55cf408e1e9
ClashLuke <luke@clashproject.com>
2019-06-15 09:47:27 +0200

    Added timestamp to registration tx

--- src/currency.c
+++ src/currency.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <math.h>
+#include <time.h>
 #include <stdlib.h>
 #include "crypto/crc.h"
 #include "crypto/blake2.h"
@@ -22,22 +23,23 @@ uint8_t* buildCoinbaseTx(uint8_t* out, uint32_t amount){
 
 uint8_t* constructRawTx(uint8_t* in, uint8_t* out, uint64_t amount, uint64_t nonce){
 	if(!nonce){
-		uint64_t* tx_64     = (uint64_t*)malloc(53); //64bit PoW, 64bit nonce, 5 byte out, 32 byte in
-		uint64_t* txNonce   = (uint64_t*)&tx[45];
+		uint64_t* tx_64     = (uint64_t*)malloc(57); //64bit nonce, 64bit PoW, 32bit timestamp, 5 byte out, 32 byte in
+		uint64_t* txNonce   = (uint64_t*)&tx[49];
 		uint64_t  powHash_0 = 0;
 		uint64_t  powHash_1 = 0;
 		uint32_t* powH_32_0 =   (uint32_t*)&powHash_1;
 		uint32_t* powH_32_1 = &((uint32_t*)&powHash_1)[1];
 		for(uint8_t i=0;i<32;i++) tx[i+ 8] = in[i];
 		for(uint8_t i=0;i< 5;i++) tx[i+40] = out[i];
-		powHash_0 = *tx_64 + tx_64[1] + tx_64[2] + tx_64[3] + tx_64[4] + tx_64[5] + tx_64[6];
+		*(uint32_t*)&tx[45] = 3 + (uint32_t)time(NULL); // A tx may not be included one minute after generation, 3 second offset added for PoW
+		powHash_0 = *tx_64 + tx_64[1] + tx_64[2] + tx_64[3] + tx_64[4] + tx_64[5] + tx_64[6] + tx_64[7];
 		*txNonce++;
 		while(1){
 			powHash_1 = powHash_0 ^ *txNonce;
 			crc32i(powH_32_0);
 			crc32i(powH_32_1);
 			*txNonce = powHash_1;
-			if(powHash_1 < 0x1000000000) break;
+			if(powHash_1 < 0x1000000000) break; // Approximately 2.5s on Xeon E2-1225v2
 		}
 	}
 	else{
