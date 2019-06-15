8c31b9655b5f1412fe6b41ca2b3705b5e24c0c57
ClashLuke <luke@clashproject.com>
2019-06-15 09:46:42 +0200

    Added last eddsa wrappers

--- src/crypto/eddsa.h
+++ src/crypto/eddsa.h
@@ -3,5 +3,7 @@
 
 void genKeyPair(uint8_t* pub, uint8_t* priv);
 void getPubFromPriv(uint8_t* pub, uint8_t* priv);
+void sign(uint8_t* out, uint8_t* pub, uint8_t* priv, uint8_t* data, uint32_t len);
+uint8_t verifySignature(uint8_t* signature, uint8_t* pub, uint8_t* data, uint32_t len);
 
 #endif
